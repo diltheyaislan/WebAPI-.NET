d0e6dcfd45f3bfd5afe001e019565896cb874ade
Dilthey <diltheyaislan@gmail.com>
2021-02-14 21:11:09 -0300

    feature: Category and Product resources

---
+++ WebAPI/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) 
+            : base(options) 
+        { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ WebAPI/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [MaxLength(60, ErrorMessage = "This field must contain between 3 and 60 characters")]
+        [MinLength(3, ErrorMessage = "This field must contain between 3 and 60 characters")]
+        public string Title { get; set; }
+    }
+}
